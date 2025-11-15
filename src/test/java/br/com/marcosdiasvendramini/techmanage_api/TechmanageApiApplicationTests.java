f1bf9f55c442732799fe133514f9572a42809762
marcosvendramini <marcosvendramini@hotmail.com>
2025-11-14 22:03:54 -0300

    fix: ajuste teste unitario

--- src/test/java/br/com/marcosdiasvendramini/techmanage_api/TechmanageApiApplicationTests.java
+++ src/test/java/br/com/marcosdiasvendramini/techmanage_api/TechmanageApiApplicationTests.java
@@ -1,13 +1,7 @@
 package br.com.marcosdiasvendramini.techmanage_api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TechmanageApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
